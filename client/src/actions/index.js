540bb597f5f64fa42e6c8ba35b695ff872c483c7
Nicky Chen <nicky.chen0@gmail.com>
2018-12-29 15:15:14 -0800

    stories fetched

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,8 +7,11 @@ export const fetchStories = () => {
       .get("https://hacker-news.firebaseio.com/v0/topstories.json")
       .then(({ data }) => {
         let id = data[0];
-        axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(({ data }) => {
-          dispatch({ type: FETCH_STORY_SUCCESS, payload: data });
+        let ids = data.slice(0, 20);
+        ids.map(id => {
+          axios.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(({ data }) => {
+            dispatch({ type: FETCH_STORY_SUCCESS, payload: data });
+          });
         });
       })
       .catch(err => {
